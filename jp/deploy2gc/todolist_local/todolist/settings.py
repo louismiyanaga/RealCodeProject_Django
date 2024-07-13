c85491228cc6b3a0a4d062f27c8f64b5c90e8c49
louismiyanaga <162401422+louismiyanaga@users.noreply.github.com>
2024-07-13 10:40:41 +0900

    modify secret key

--- jp/deploy2gc/todolist_local/todolist/settings.py
+++ jp/deploy2gc/todolist_local/todolist/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-owveyg7mf!iehis$6y5iu@m+4k_%m$i8s2()-$#0gg3z*pfd)k'
+SECRET_KEY = 'django-insecure-owveyg7mf!iehis$6y5iu@m+4k_%m$i8s2-$0gg3z*pfdk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
